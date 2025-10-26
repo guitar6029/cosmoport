38ec9c0bd71b90ee873072e670930e15294a1ddb
Josh <jsbusiness305@gmail.com>
2025-10-26 16:58:04 -0400

    added Hero view

--- client/src/views/Hero.vue
+++ client/src/views/Hero.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+import stationimg from '../assets/img/station_1.jpg'
+</script>
+
 <template>
-    <h1>Hero</h1>
+    <main class="hero bg-base-200 min-h-screen flex flex-row items-center justify-center gap-2">
+        <div class="hero-content text-center">
+            <div class="max-w-md">
+                <h1 class="text-5xl font-bold">CostmoPort</h1>
+                <p class="py-6">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In, aspernatur? Et error eius, facilis aut expedita eaque tenetur, ea non accusamus distinctio natus corrupti eveniet nisi, voluptatum similique aperiam saepe!
+                </p>
+                <button class="btn btn-primary">Get Started</button>
+            </div>
+        </div>
+        <div class="hero-content text-center">
+            <div class="max-w-md">
+                <img :src="stationimg" class="rounded-lg shadow-2xl" />
+            </div>
+        </div>
+    </main>
 </template>
