c9526b64472e3b5f8b8bd490019e11614779c06e
Josh <jsbusiness305@gmail.com>
2025-10-27 15:57:03 -0400

    changed order

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,14 +5,16 @@ import  router  from "./router";
 import { createPinia } from "pinia";
 import { useSessionUser } from "./store/useSessionUser";
 
-const pinia = createPinia();
 const app = createApp(App);
+const pinia = createPinia();
 app.use(pinia);
-app.use(router);
 
 
 // initialze Supabase session 
 const sessionUser = useSessionUser();
-sessionUser.init();
+// calls supabsae 
+await sessionUser.init(); 
 
+app.use(router);
+await router.isReady();
 app.mount("#app");
