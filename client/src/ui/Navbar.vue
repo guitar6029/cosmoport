e94e143d05fc8e1836a6d416f5cbab8f790a0ae1
Josh <jsbusiness305@gmail.com>
2025-10-27 12:43:20 -0400

    added dynamic linking for user (if user is logged in / not logged in )
    added Account view to navbar, and added Account view

--- client/src/ui/Navbar.vue
+++ client/src/ui/Navbar.vue
@@ -1,19 +1,33 @@
 <script lang="ts" setup>
+import { CircleUser as Account } from "lucide-vue-next"
 import ThemeToggler from './ThemeToggler.vue';
 import { RouterLink } from 'vue-router';
+import { useSessionUser } from '../store/useSessionUser';
+import { computed } from 'vue';
+
+const sessionUserStore = useSessionUser();
+const isAuthed = computed(() => sessionUserStore.isAuthed);
+
 </script>
 
 <template>
     <header class="nav bg-base-300">
         <nav class="flex flex-row items-center justify-between p-4">
-            <div class="text-2xl font-bold">CostmoPort</div>
             <div class="flex items-center gap-2">
-                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/">
+                    <div class="text-2xl font-bold">CostmoPort</div>
+                </RouterLink>
                 <RouterLink to="/flights">Flights</RouterLink>
-                <RouterLink to="/login">Login</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </div>
+            <div class="flex items-center gap-2">
+                <RouterLink to="/login" v-if="!isAuthed">Login</RouterLink>
+                <RouterLink to="/account" v-if="isAuthed">
+                    <Account />
+                </RouterLink>
                 <!-- save for later when dashboard is implemented and user auth is implemented -->
                 <!-- <RouterLink to="/dashnoard">Dashboard</RouterLink> -->
-                <RouterLink to="/about">About</RouterLink>
+
                 <ThemeToggler />
             </div>
         </nav>
---
+++ client/src/views/Account.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useSessionUser } from '../store/useSessionUser';
+import { onMounted } from 'vue';
+
+</script>
+
+
+<template>
+    <section class="flex flex-col gap-2">
+        <h1>Account</h1>
+        <div class="avatar">
+            <div class="w-24 rounded">
+                <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+            </div>
+        </div>
+    </section>
+</template>
