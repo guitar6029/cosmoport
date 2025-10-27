070549dcad1d4d2875b93706fefd7c53921f7311
Josh <jsbusiness305@gmail.com>
2025-10-27 17:16:50 -0400

    added dashboard view

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useSessionUser } from '../store/useSessionUser';
+
+const store = useSessionUser();
+
+const role = computed(() => store.role);
+
+</script>
+
+
 <template>
     <h1>Dashboard</h1>
+    <span>User Current Role : {{ role }}</span>
 </template>
