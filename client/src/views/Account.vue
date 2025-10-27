9c77ec20d8dac8779cf0706826090cba37c73c84
Josh <jsbusiness305@gmail.com>
2025-10-27 12:50:26 -0400

    updated Account view

--- client/src/views/Account.vue
+++ client/src/views/Account.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
 import { useSessionUser } from '../store/useSessionUser';
-import { onMounted } from 'vue';
+import { computed } from 'vue';
+import type { Profile } from '../types/Profile';
+
+//get user from the store
+const sessionUserStore = useSessionUser();
+const profile = computed<Profile | null>(() => sessionUserStore.profile);
 
 </script>
 
 
 <template>
     <section class="flex flex-col gap-2">
-        <h1>Account</h1>
+        <h1>Hello {{ profile?.display_name ?? 'User' }}</h1>
         <div class="avatar">
             <div class="w-24 rounded">
                 <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
             </div>
         </div>
+        <button class="btn btn-primary w-fit">Change Avatar</button>
     </section>
 </template>
