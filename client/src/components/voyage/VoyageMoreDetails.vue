bbe4f0cc6f624ea6d20796df7c53b9086dbe6ede
Josh <jsbusiness305@gmail.com>
2025-10-29 10:51:30 -0400

    updated VoyageMoreDetails

--- client/src/components/voyage/VoyageMoreDetails.vue
+++ client/src/components/voyage/VoyageMoreDetails.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import type { Voyage } from '../../types/Voyage';
 import { getDifficultyClass } from '../../utils/difficultyUtils'
-import { Clock, Eclipse } from 'lucide-vue-next';
+import { Clock, Eclipse, DollarSign } from 'lucide-vue-next';
 import UiIcon from '../../ui/UiIcon.vue';
 
 defineProps<{
@@ -10,33 +10,42 @@ defineProps<{
 </script>
 
 <template>
-    <p class="text-xl border-2 border-primary rounded-xl p-2">{{ selectedVoyage?.description }}</p>
-    <div :class="['badge badge-xl', getDifficultyClass(selectedVoyage?.difficulty as Voyage['difficulty'])]">
-        {{ selectedVoyage?.difficulty }}</div>
-    <div class="text-xl flex items-center gap-2">
-        <UiIcon :icon="Clock" size="lg" customClass="text-primary" />
-        <span>Duration:</span>
-        <span class="font-bold">{{ selectedVoyage?.durationMinutes }} minutes</span>
-    </div>
-
-    <div class="mt-2 mb-2 flex flex-col gap-2">
-        <div class="text-xl flex items-center gap-2">
-            <UiIcon :icon="Eclipse" size="lg" customClass="text-primary" />
-            <span>Origin:</span>
-            <span class="font-bold">{{ selectedVoyage?.origin }}</span>
+    <div class="flex flex-col items-center justify-center md:flex-row md:items-start gap-2">
+        <!-- left side on large screens -->
+        <div class="flex flex-col gap-4">
+            <p class="text-xl">{{ selectedVoyage?.description }}</p>
+            <div :class="['badge badge-xl', getDifficultyClass(selectedVoyage?.difficulty as Voyage['difficulty'])]">
+                {{ selectedVoyage?.difficulty }}</div>
+            <div class="text-xl flex items-center gap-2">
+                <UiIcon :icon="Clock" size="lg" customClass="text-primary" />
+                <span>Duration</span>
+                <span class="font-bold badge badge-xl badge-outline">{{ selectedVoyage?.durationMinutes }}
+                    minutes</span>
+            </div>
+            <div class="text-xl flex items-center gap-2">
+                <UiIcon :icon="Eclipse" size="lg" customClass="text-primary" />
+                <span>Origin:</span>
+                <span class="font-bold badge badge-xl badge-outline">{{ selectedVoyage?.origin }}</span>
+            </div>
+            <div class="text-xl flex items-center gap-2">
+                <UiIcon :icon="Eclipse" size="lg" customClass="text-primary" />
+                <span>Destination</span>
+                <span class="font-bold badge badge-xl badge-outline">{{ selectedVoyage?.destination }}</span>
+            </div>
+            <div class="text-xl flex items-center gap-2">
+                <UiIcon :icon="DollarSign" size="lg" customClass="text-primary" />
+                <span>Reward</span>
+                <span class="font-bold badge badge-xl badge-outline">{{ selectedVoyage?.reward }}</span>
+            </div>
+            <div class="text-xl flex items-center gap-2">
+                <span>Recommended Ship</span>
+                <span class="font-bold badge badge-xl badge-outline">{{ selectedVoyage?.recommendedShip }}</span>
+            </div>
         </div>
-        <div class="text-xl flex items-center gap-2">
-            <UiIcon :icon="Eclipse" size="lg" customClass="text-primary" />
-            <span>Destination: </span>
-            <span class="font-bold">{{ selectedVoyage?.destination }}</span>
+        <!-- right side on large screens -->
+        <div class="min-w-sm max-w-sm">
+            <img class="rounded-xl" :src="selectedVoyage?.imageUrl" :alt="`${selectedVoyage?.name} image`">
         </div>
-    </div>
 
-    <div class="mt-2 mb-2 flex flex-col gap-2">
-
-        <div class="text-xl flex items-center gap-2">
-            <span>Recommended Ship:</span>
-            <span class="font-bold">{{ selectedVoyage?.recommendedShip }}</span>
-        </div>
     </div>
 </template>
