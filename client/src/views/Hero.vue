74ec1c7d731bcd077deaf9a4a558270c4c7090df
Josh <jsbusiness305@gmail.com>
2025-10-28 20:54:25 -0400

    added custom space font , update Hero view

--- client/src/views/Hero.vue
+++ client/src/views/Hero.vue
@@ -3,22 +3,14 @@ import stationimg from '../assets/img/station_1.jpg'
 </script>
 
 <template>
-    <section class="hero bg-base-200 flex flex-row items-center justify-center gap-2">
-        <div class="hero-content text-center">
-            <div class="max-w-md">
-                <h1 class="text-5xl font-bold">CostmoPort</h1>
-                <p class="py-6">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In, aspernatur? Et error eius, facilis aut
-                    expedita eaque tenetur, ea non accusamus distinctio natus corrupti eveniet nisi, voluptatum
-                    similique aperiam saepe!
-                </p>
-                <button class="btn btn-primary">Get Started</button>
-            </div>
+    <section class="hero bg-base-200 flex flex-col md:flex-row items-center justify-center gap-6">
+        <div class="w-full md:max-w-xl flex flex-col gap-12 text-center">
+            <h1 class="text-2xl md:text-5xl font-bold font-space">CostmoPort</h1>
+            <button class="btn border-2 border-primary">Check Flights</button>
         </div>
-        <div class="hero-content text-center">
-            <div class="max-w-md">
-                <img :src="stationimg" alt="space station" class="rounded-lg shadow-2xl" />
-            </div>
+        <div class="w-full md:max-w-xl">
+            <img :src="stationimg" alt="space station"
+                class="rounded-lg shadow-2xl w-full md:max-w-3xl transition duration-300 ease-in hover:scale-105" />
         </div>
     </section>
 </template>
