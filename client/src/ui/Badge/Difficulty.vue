ca81791e43d11200c9d80d97d9762698ac9ce774
Josh <jsbusiness305@gmail.com>
2025-10-29 14:06:25 -0400

    added new ui Badge for Difficulty

---
+++ client/src/ui/Badge/Difficulty.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { Voyage } from '../../types/Voyage';
+import { getDifficultyClass } from '../../utils/difficultyUtils'
+defineProps<{
+    difficulty: Voyage['difficulty']
+}>();
+
+</script>
+
+<template>
+    <div :class="['badge badge-xl', getDifficultyClass(difficulty)]">
+        {{ difficulty }}
+    </div>
+</template>
