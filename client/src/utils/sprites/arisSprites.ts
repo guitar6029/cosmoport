ac87a952f6f5b8dc53651ce502674d0ab3875444
Josh <jsbusiness305@gmail.com>
2025-10-31 20:11:33 -0400

    added arisSprites util, tested that the image works

---
+++ client/src/utils/sprites/arisSprites.ts
@@ -0,0 +1,31 @@
+
+export const arisSprites = {
+  neutral: new URL(
+    "../../assets/img/characters/aris/aris_neutral.jpg",
+    import.meta.url
+  ).href,
+  talkA: new URL(
+    "../../assets/img/characters/aris/aris_talk_a.jpg",
+    import.meta.url
+  ).href,
+  talkB: new URL(
+    "../../assets/img/characters/aris/aris_talk_b.jpg",
+    import.meta.url
+  ).href,
+  blink: new URL(
+    "../../assets/img/characters/aris/aris_blink.jpg",
+    import.meta.url
+  ).href,
+  smile: new URL(
+    "../../assets/img/characters/aris/aris_smile.jpg",
+    import.meta.url
+  ).href,
+  concern: new URL(
+    "../../assets/img/characters/aris/aris_concern.jpg",
+    import.meta.url
+  ).href,
+  glitch: new URL(
+    "../../assets/img/characters/aris/aris_glitch.jpg",
+    import.meta.url
+  ).href,
+} as const;
--- client/src/views/Outpost.vue
+++ client/src/views/Outpost.vue
@@ -1,3 +1,8 @@
+<script lang="ts" setup>
+import {arisSprites} from "../utils/sprites/arisSprites"
+</script>
+
 <template>
     <h1>Outpost</h1>
+    <img :src="arisSprites.neutral" alt="Face of Aris, our character, helper to the player ">
 </template>
