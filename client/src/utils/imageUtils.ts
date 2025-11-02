88bc5b1b86e14ff7b862e52e4aa01bf4a128a11d
Josh <jsbusiness305@gmail.com>
2025-11-02 09:54:00 -0500

    updated Journal UI, changed json to ts for the itemsData added a utility class trns in style.css

--- client/src/utils/imageUtils.ts
+++ client/src/utils/imageUtils.ts
@@ -1,5 +1,27 @@
+/**
+ * Returns an object containing URLs of ship images based on its ID.
+ * The object contains three properties: default, side1, and side2.
+ * @param {string} key The ID of the ship.
+ * @returns {Object} An object containing URLs of ship images.
+ * @example
+ * const images = getShipImages('starling-courier');
+ * console.log(images.default); // URL of the default ship image
+ * console.log(images.side1); // URL of the side1 ship image
+ * console.log(images.side2); // URL of the side2 ship image
+ */
 export const getShipImages = (key: string) => ({
   default: new URL(`../assets/img/ships/${key}.png`, import.meta.url).href,
   side1: new URL(`../assets/img/ships/${key}-1.png`, import.meta.url).href,
   side2: new URL(`../assets/img/ships/${key}-2.png`, import.meta.url).href,
 });
+
+/**
+ * Returns the URL of an item image based on its ID.
+ * @param {string} id The ID of the item.
+ * @returns {string} The URL of the item image.
+ */
+export const getItemImages = (id: string | null) => {
+  if (!id) return "";
+  const url = new URL(`../assets/img/items/${id}.jpg`, import.meta.url);
+  return url.toString();
+};
--- client/src/views/Journal.vue
+++ client/src/views/Journal.vue
@@ -1,5 +1,54 @@
 <script lang="ts" setup>
+import { ref } from 'vue'
+import { itemsData } from '@/assets/itemsData'
+import { getItemImages } from '@/utils/imageUtils';
+import type { Item } from '@/types/Item';
+
+
+const currentItemView = ref(itemsData[0]);
+
+const handleSelectedItem = (index: number) => {
+    const foundItem = itemsData.find((item: Item) => item.id === itemsData[index]?.id)
+    if (foundItem) {
+        currentItemView.value = foundItem
+    }
+}
+
 </script>
 <template>
-    <h1>Journal</h1>
+    <div class="flex flex-col items-center w-full gap-2">
+        <section>
+            <h1 class="title-main">Journal</h1>
+            <!-- <h1 class="title-section">Items</h1> -->
+            <div class="flex flex-row gap-1 glass">
+
+                <div class="flex w-2xl flex-col gap-2 rounded-2xl">
+
+                    <Transition name="fade" mode="out-in" appear>
+                        <div :key="currentItemView?.id" class="flex flex-col gap-2 p-2 w-full md:max-w-150">
+                            <h1 class="font-bold text-4xl font-sci-fi text-accent bg-primary/75 p-2 w-fit">{{
+                                currentItemView?.name }}</h1>
+                            <img :src="getItemImages(currentItemView?.id ?? null)" :alt="currentItemView?.name"
+                                class="w-50 h-50  rounded-xl">
+                            <p class="font-bold w-full text-md text-2xl">{{ currentItemView?.description }}</p>
+                            <span>{{ currentItemView?.value }}</span>
+                        </div>
+                    </Transition>
+                </div>
+                <div class="flex max-w-3xl flex-wrap items-center gap-2 rounded-2xl max-h-100 overflow-scroll">
+                    <TransitionGroup name="fade" tag="div" appear
+                        class="flex items-center justify-center flex-wrap gap-4 p-2">
+                        <div v-for="item, index in itemsData" :key="item.id" @click="handleSelectedItem(index)"
+                            :style="{ transitionDelay: `${index * 100}ms` }"
+                            class="flex flex-col gap-2 p-2 w-full md:max-w-40 cursor-pointer items-center bg-secondary/55 rounded-2xl">
+                            <img :src="getItemImages(item.id)" :alt="item.name"
+                                class="w-25 h-25 trns hover:scale-110 rounded-xl">
+                            <h1 class="font-bold text-md">{{ item.name }}</h1>
+                        </div>
+                    </TransitionGroup>
+                </div>
+
+            </div>
+        </section>
+    </div>
 </template>
